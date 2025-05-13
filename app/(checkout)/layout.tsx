0d4abd57ad1a7ecf77c7eb94017d93afffc1b2f0
Dolo323 <jxjdhd7@gmail.com>
2025-05-13 03:15:59 +0300

    create app

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,6 +1,6 @@
-
 import { Metadata } from 'next';
 import { Container, Header } from '../../shared/components/shared';
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: 'Next Pizza | Корзина',
@@ -10,10 +10,12 @@ export const metadata: Metadata = {
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen bg-[#F4F1EE]"> 
-    <Container>
-    <Header hasSearch={false} hasCart={false} className='border-gray-200'/>
-    {children} 
-    </Container>
+      <Container>
+        <Suspense fallback={null}>
+          <Header hasSearch={false} hasCart={false} className='border-gray-200'/>
+        </Suspense>
+        {children} 
+      </Container>
     </main>
   );
 }
